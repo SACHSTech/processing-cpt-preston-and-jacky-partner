d6f58da6487788b07f1744c0a0ba3a26c934a47b
Jacky-Ho0429 <jacky.ho25@ycdsbk12.ca>
2023-12-27 21:36:01 -0500

    Testing Session 1

--- Sketch1.java
+++ Sketch1.java
@@ -4,27 +4,39 @@ import processing.core.PImage;
 public class Sketch1 extends PApplet {
 
   PImage[] imgLevel;
+  PImage[] imgLevelCollision;
+
   int intNumLevels = 1;
+
+  float fltCollisionColor;
   
   public void settings() {
     
-    size(1200, 720);
+    size(1200, 672);
 
   }
 
   public void setup() {
 
     imgLevel = new PImage[intNumLevels];
+    imgLevelCollision = new PImage[intNumLevels];
 
     for (int i = 0; i < intNumLevels; i++) {
       imgLevel[i] = loadImage("levels/level" + i + ".png"); 
     }
-    
+
+    for (int i = 0; i < intNumLevels; i++) {
+      imgLevelCollision[i] = loadImage("levels/collision/levelCollision" + i + ".png"); 
+    }
   }
 
   
   public void draw() {
+    image(imgLevelCollision[0], 0, 0);
+    fltCollisionColor = get(mouseX,mouseY);
+    System.out.print(fltCollisionColor);
 	  image(imgLevel[0], 0, 0);
+
   }
   
   // define other methods down here.
