108de75c7c3c1d9d7c6bf614fbeb4b0a8e76a386
Preston-Wong <you@example.com>
2024-01-02 17:58:12 -0500

    commented the platfomer

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ class Main {
   public static void main(String[] args) {
     
     String[] processingArgs = {"MySketch"};
+	
 	//Sketch mySketch = new Sketch();  //comment this out to run the other sketch files
 	 // platformer mySketch = new platformer();  // uncomment this to run this sketch file
 	  escape_room mySketch2 = new escape_room();  // uncomment this to run this sketch file
